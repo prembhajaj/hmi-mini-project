7a66582e9c98215b1b952b1600a44d4e29458466
Pratik Panchal <pratik.sp.1112@gmail.com>
2021-04-19 21:04:36 +0530

    Summary chart

--- Mini project/script.js
+++ Mini project/script.js
@@ -1,6 +1,8 @@
 hideAll();
 document.getElementById("login_div").style.display="block";
-
+var income = 0;
+var expense = 0;
+drawChart();
 
 function hideAll(){
     var divs = document.getElementsByTagName("div");
@@ -25,3 +27,37 @@ function onChange(showDiv){
     document.getElementById(showDiv).style.display="block";
 }
 
+function drawChart(){
+    document.getElementById("income_span").innerText = income;
+    document.getElementById("expense_span").innerText = expense;
+    
+    var temp = income + expense;
+    var income_percent = 0;
+    var expense_percent = 0;
+
+    if (temp > 0){
+        income_percent = (100*(income/temp));
+        expense_percent = (100*(expense/temp));
+    }
+
+    alert(income_percent+"% ("+income+") Income, " + expense_percent+"% ("+expense+") Expense");
+
+    document.getElementById("income_chart").style.width = income_percent+"%";
+    document.getElementById("expense_chart").style.width = expense_percent+"%";
+}
+
+function incomeAdd(){
+    var temp = +document.getElementById("income_amt_text").value;
+    document.getElementById("income_amt_text").value = "";
+    document.getElementById("income_desc_text").value = "";
+    income += temp;
+    drawChart();
+}
+
+function expenseAdd(){
+    var temp = +document.getElementById("expense_amt_text").value;
+    document.getElementById("expense_amt_text").value = "";
+    document.getElementById("expense_desc_text").value = "";
+    expense += temp;
+    drawChart();
+}
